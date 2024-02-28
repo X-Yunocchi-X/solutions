3276d4a2b2e415326baed53110397f066bb2d654
Yunocchi <106807558+X-Yunocchi-X@users.noreply.github.com>
2024-02-28 22:48:35 +0800

    format code

--- leetcode/72.go
+++ leetcode/72.go
@@ -19,7 +19,7 @@ func minDistance(word1 string, word2 string) int {
 			if word1[i] == word2[j] {
 				dp[i+1][j+1] = dp[i][j]
 			}
-			dp[i+1][j+1] = min(dp[i+1][j+1], dp[i][j+1]+1, dp[i+1][j]+1, dp[i][j]+1)
+			dp[i+1][j+1] = min(dp[i+1][j+1], min(dp[i][j], dp[i][j+1], dp[i+1][j])+1)
 		}
 	}
 
